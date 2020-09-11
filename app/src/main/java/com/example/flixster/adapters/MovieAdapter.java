8866ff2e8dc0ba308854e8f3998dee01cb580448
Brandon Ferguson <bfergus5@bulldogs.aamu.edu>
2020-09-11 15:45:16 -0500

    Flixster App part 2

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.Movie;
 import com.example.flixster.R;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -50,20 +56,22 @@ public MovieAdapter(Context context, List <Movie> movies){
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
 
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvTitle =itemView.findViewById(R.id.tvTitle);
-            tvOverview =itemView.findViewById(R.id.tvOverview);
+            tvTitle = itemView.findViewById(R.id.container);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
-
+            container = itemView.findViewById(R.id.container);
 
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(Movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
@@ -71,10 +79,23 @@ public MovieAdapter(Context context, List <Movie> movies){
             if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 //then imageUrl =BACK DROP IMAGE
                 imageUrl = movie.getBackdropPath();
-            }else {
-            // ELSE IMAGEuRL = POSTER Image
-            imageUrl =movie.getPosterPath();
-            Glide.with(context).load(imageUrl).into(ivPoster);
+            } else {
+                // ELSE imageURL = POSTER Image
+                imageUrl = movie.getPosterPath();
+            }
+                Glide.with(context).load(imageUrl).into(ivPoster);
+                // 1.)Register Click Listener on the whole row
+                container.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 2.Navigate to a new activity on tap
+                        Toast.makeText(context,movie.getTitle(), Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(context, DetailActivity.class);
+                        i.putExtra("title",movie.getTitle());
+                        i.putExtra( "movie", Parcels.wrap(movie));
+                        context.startActivity(i);
+                    }
+                });
+            }
         }
     }
-}
